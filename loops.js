81e1ee65d6c678889e67403670ca57e7e6ea9d9b
Charles Cusson <charlescusson@gmail.com>
2020-04-01 21:41:51 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,6 +18,18 @@ while (number > 0){
   return "done"
 }
 
+function doWhileLoop(num)­{
+let i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
+}
 /*
 function doWhileLoop(num)­{
 do{console.log("I run once regardless.")}
