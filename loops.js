c9604e4951f3ceba96a7f7b4692c47bf5f54f46d
Charles Cusson <charlescusson@gmail.com>
2020-04-01 20:28:37 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i===1){
+      array.push("I am 1 strange loop.")
+    }
+    else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
+}
+/*If your i value is 1, add the string "I am 1 strange loop."
+If your i value is anything else, add the string "I am ${i} strange loops."
+Remember flow control with if and else? And how do we interpolate i?
+
+Once the loop has finished, return the array full of strings.
+*/
